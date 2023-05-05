8089339fc019f4f927b2a65c3db9012e35d3469a
koyama-c <koyama@linkedge.jp>
2023-05-05 12:56:53 +0900

    webhookの検証完了

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # messaging-api
+
+外部公開するために
+
+- ngrokのインストール
+```
+brew install --cask ngrok
+```
+- ngrokのアカウント作成
+- 認証トークンの取得
+- ngrokの認証
+```
+ngrok authtoken <your_auth_token>
+```
+- ngrokの起動
+```
+ngrok http 3030
+```
---
+++ index.js
@@ -0,0 +1,75 @@
+// .envファイルから環境変数の読み込み
+require('dotenv').config()
+const https = require("https")
+const express = require("express")
+const app = express()
+
+const PORT = process.env.PORT
+const TOKEN = process.env.LINE_ACCESS_TOKEN
+
+app.use(express.json())
+app.use(express.urlencoded({
+  extended: true
+}))
+
+// ヘルスチェックが失敗しないように
+app.get("/", (req, res) => {
+  res.sendStatus(200)
+  console.log('Hello World!')
+})
+
+// postリクエストをルーティングする→ボットサーバがきたリクエストにどのように対応するのかを設定
+app.post("/webhook", function(req, res) {
+  res.send("HTTP POST request sent to the webhook URL!")
+
+  // 応答トークンを受け取る
+  const dataString = JSON.stringify({
+    replyToken: req.body.events[0].replyToken,
+    messages: [
+      {
+        "type": "text",
+        "text": "Hello, user"
+      },
+      {
+        "type": "text",
+        "text": "May I help you?"
+      }
+    ]
+  })
+  
+  // ユーザの返信をpostするためのリクエストヘッダの作成
+  const headers = {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + TOKEN
+  }
+  
+  // node.jsの仕様（エンドポイントのホスト名とパスなどのオプションを設定）
+  const webhookOptions = {
+    "hostname": "api.line.me",
+    "path": "/v2/bot/message/reply",
+    "method": "POST",
+    "headers": headers,
+    "body": dataString
+  }
+
+  const request = https.request(webhookOptions, (res) => {
+    res.on("data", (d) => {
+      process.stdout.write(d)
+    })
+
+    // リクエスト送信時のエラーをキャッチ
+    request.on("error", (err) => {
+      console.error(err)
+    })
+  
+    // 定義したリクエストの送信
+    request.write(dataString)
+    request.end()
+  })
+
+
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening at http://localhost:${PORT}`)
+})
